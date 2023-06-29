da5c8cd982749c4afa94e3db7e06d5bd0936329b
Usama Aslam <awesomeusama920@gmail.com>
2023-06-29 22:12:32 +0500

    new

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,9 +10,6 @@ class LoginController extends Controller
 {
     public function TryLogin(Request $request)
     {
-        return response()->json([
-            'status' => 'error', 'messsage' => 'Invalid Credentials!'
-        ]);
         $username = $request->username;
         $password = $request->password;
         $account = DB::table('account')->where(['username' => $username])->first();
