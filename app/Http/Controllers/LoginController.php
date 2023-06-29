e58e9a99e1bc73cd615bd24d2607ec0ece84c7ef
Usama Aslam <awesomeusama920@gmail.com>
2023-06-29 21:20:13 +0500

    new

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,24 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\VisitorStatsSessions;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    public function TryLogin()
+    public function TryLogin(Request $request)
     {
-        $username = "";
-        $password = "";
+        $username = $request->username;
+        $password = $request->password;
         $account = DB::table('account')->where(['username' => $username])->first();
-        $checkPassword = Hash::check($password, $account->password);
-        if ($checkPassword) {
-            return response()->json([
-                'status' => 'success', 'username' => $account->username
-            ]);
+        if ($account) {
+            $checkPassword = Hash::check($password, $account->password);
+            if ($checkPassword) {
+                return response()->json([
+                    'status' => 'success', 'username' => $account->username
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 'error', 'messsage' => 'Invalid Credentials!'
+                ]);
+            }
         } else {
             return response()->json([
                 'status' => 'error', 'messsage' => 'Invalid Credentials!'
