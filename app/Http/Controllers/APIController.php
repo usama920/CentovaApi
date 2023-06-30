2019c95ef9b8fa0c5403f2c557d8d889966b7dc0
Usama Aslam <awesomeusama920@gmail.com>
2023-06-30 16:28:45 +0500

    new

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -22,26 +22,30 @@ class APIController extends Controller
 
     public function StatisticsListeners(Request $request)
     {
-        $subDays = $request->days ? $request->days : 14;
-        $account_id = $request->account_id ? $request->account_id : null;
-        $subDaysTime = Carbon::today()->subDays($subDays);
-        $visitorSessions = VisitorStatsSessions::where(['accountid' => $account_id])->where('starttime', '>=', $subDaysTime)->get();
-        $total_seconds = 0;
-        $total_data = 0;
+        try {
+            $subDays = $request->days ? $request->days : 14;
+            $account_id = $request->account_id ? $request->account_id : null;
+            $subDaysTime = Carbon::today()->subDays($subDays);
+            $visitorSessions = VisitorStatsSessions::where(['accountid' => $account_id])->where('starttime', '>=', $subDaysTime)->get();
+            $total_seconds = 0;
+            $total_data = 0;
 
-        foreach ($visitorSessions as $session) {
-            $total_seconds += $session->duration;
-            $total_data += $session->bandwidth;
+            foreach ($visitorSessions as $session) {
+                $total_seconds += $session->duration;
+                $total_data += $session->bandwidth;
+            }
+            $total_minutes = round($total_seconds / 60);
+            $total_hours = round($total_minutes / 60, 1);
+            $total_sessions = count($visitorSessions);
+
+            $average_data_transfer = $total_data > 0 ? format_size($total_data / $total_sessions) : format_size($total_data);
+            $uniqueIpSessions = VisitorStatsSessions::where(['accountid' => $account_id])->groupBy('ipaddress')->count();
+
+            $uniqueCountrySessions = VisitorStatsSessions::where(['accountid' => $account_id])->groupBy('country')->count();
+
+            return response()->json(['total_minutes' => $total_minutes, 'total_hours' => $total_hours, 'total_sessions' => $total_sessions, 'uniqueIpSessions' => $uniqueIpSessions, 'uniqueCountrySessions' => $uniqueCountrySessions, 'total_data_transfer' => format_size($total_data), 'average_data_transfer' => $average_data_transfer]);
+        } catch (\Throwable $th) {
+            return response()->json($th);
         }
-        $total_minutes = round($total_seconds / 60);
-        $total_hours = round($total_minutes / 60, 1);
-        $total_sessions = count($visitorSessions);
-
-        $average_data_transfer = $total_data > 0 ? format_size($total_data / $total_sessions) : format_size($total_data);
-        $uniqueIpSessions = VisitorStatsSessions::where(['accountid' => $account_id])->groupBy('ipaddress')->count();
-
-        $uniqueCountrySessions = VisitorStatsSessions::where(['accountid' => $account_id])->groupBy('country')->count();
-
-        return response()->json(['total_minutes' => $total_minutes, 'total_hours' => $total_hours, 'total_sessions' => $total_sessions, 'uniqueIpSessions' => $uniqueIpSessions, 'uniqueCountrySessions' => $uniqueCountrySessions, 'total_data_transfer' => format_size($total_data), 'average_data_transfer' => $average_data_transfer]);
     }
 }
