263206c2966c3f8dab73dbfcd6f5aaeada8f8bd4
Usama Aslam <awesomeusama920@gmail.com>
2023-07-29 19:49:54 +0500

    new

--- app/helpers.php
+++ app/helpers.php
@@ -9,16 +9,18 @@ function prx($value)
 
 function format_size($size)
 {
-    if ($size >= 1048576) {
-        $size = number_format($size / 1048576, 2) . ' GB';
+    if ($size >= 1073741824) {
+        $size = number_format($size / 1073741824, 2) . ' GB';
+    } elseif ($size >= 1048576) {
+        $size = number_format($size / 1048576, 2) . ' MB';
     } elseif ($size >= 1024) {
-        $size = number_format($size / 1024, 2) . ' MB';
+        $size = number_format($size / 1024, 2) . ' KB';
     } elseif ($size > 1) {
-        $size = $size . ' KB';
+        $size = $size . ' Bytes';
     } elseif ($size == 1) {
-        $size = $size . ' KB';
+        $size = $size . ' Byte';
     } else {
-        $size = '0 KB';
+        $size = '0 Bytes';
     }
     return $size;
 }
