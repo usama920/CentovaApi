a557b67554e67ffe486929a8d6d009d028f984fc
Usama Aslam <awesomeusama920@gmail.com>
2023-08-04 20:48:03 +0500

    new

--- app/Http/Controllers/WidgetController.php
+++ app/Http/Controllers/WidgetController.php
@@ -19,4 +19,23 @@ class WidgetController extends Controller
 
         return response()->json(['status' => 'success']);
     }
+
+    public function UpdateSongRequests(Request $request)
+    {
+        $request->validate([
+            'account_id' => 'required',
+            'automatically_queue' => 'required',
+            'request_delay' => 'required',
+            'request_probability' => 'required',
+            'email_unknown_requests' => 'required'
+        ]);
+        Account::where(['id' => $request->account_id])->update([
+            'autoqueuerequests' => $request->automatically_queue,
+            'requestdelay' => $request->request_delay,
+            'requestprobability' => $request->request_probability,
+            'emailunknownrequests' => $request->email_unknown_requests
+        ]);
+
+        return response()->json(['status' => 'success']);
+    }
 }
