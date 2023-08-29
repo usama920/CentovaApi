1b841336e8debec7ac5c1975f9f7b8771e6e1272
Usama Aslam <awesomeusama920@gmail.com>
2023-08-29 23:18:10 +0500

    new

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\DjautoController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\StatisticsController;
 use App\Http\Controllers\WidgetController;
+use App\Models\Track;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,14 +21,52 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/statistics/tracks', function () {
-    $user = 'admin';
-    $pass = 'RSRnet2018';
-    $url  = "http://$user:$pass@51.81.208.185:8800/admin.cgi?sid=1&mode=viewxml&page=3";
+Route::post('/statistics/tracks', function (Request $request) {
+    $request->validate([
+        'account_id' => 'required'
+    ]);
+    $subDays = $request->days ? $request->days : 14;
+    $account_id = $request->account_id ? $request->account_id : null;
+    $subDaysTime = Carbon::today()->subDays($subDays);
 
-    $obj  = json_decode(json_encode(simplexml_load_file($url)));
-    print_r($obj);
-    // return view('welcome');
+    $startDate = null;
+    $endDate = null;
+    if (isset($request->from_date) && $request->from_date != null && isset($request->to_date) && $request->to_date != null) {
+        $startDate = Carbon::createFromFormat('Y-m-d', $request->from_date)->startOfDay();
+        $endDate = Carbon::createFromFormat('Y-m-d', $request->to_date)->endOfDay();
+    }
+
+    if ($startDate && $endDate) {
+        $playbackStats = DB::table('playbackstats_tracks')->whereBetween('starttime', [$startDate, $endDate])->where(['accountid' => $account_id])->orderBy('listeners', 'DESC')->orderBy('duration', 'DESC')->get();
+    } else {
+        $playbackStats = DB::table('playbackstats_tracks')->where('starttime', '>=', $subDaysTime)->where(['accountid' => $account_id])->orderBy('listeners', 'DESC')->orderBy('duration', 'DESC')->get();
+    }
+
+    $total_tracks = count($playbackStats);
+    $total_duration = 0;
+    $average_length = 0;
+    $peak_listeners = 0;
+    $peak_track = null;
+    $peak_time = null;
+    if ($total_tracks > 0) {
+        $total_duration = $playbackStats->sum('duration');
+        $average_length = round($total_duration / $total_tracks);
+        $peak_listeners = $playbackStats[0]->listeners;
+        $peak_track = $playbackStats[0]->name;
+        $peak_time = $playbackStats[0]->starttime;
+    }
+
+    $user_Tracks = Track::where(['accountid' => $account_id])->get();
+    $unique_tracks = count($user_Tracks);
+
+    if ($startDate && $endDate) {
+        $topTracksByPlayback = DB::table('playbackstats_tracks')->whereBetween('starttime', [$startDate, $endDate])->where(['accountid' => $account_id])->groupBy('name')->select('name', DB::raw('count(*) as playbacks'))->orderBy('playbacks', 'DESC')->limit(10)->get();
+        $topTracksByAirTime = DB::table('playbackstats_tracks')->whereBetween('starttime', [$startDate, $endDate])->where(['accountid' => $account_id])->groupBy('name')->select('name', DB::raw('sum(duration) as totalDuration'))->orderBy('totalDuration', 'DESC')->limit(10)->get();
+    } else {
+        $topTracksByPlayback = DB::table('playbackstats_tracks')->where('starttime', '>=', $subDaysTime)->where(['accountid' => $account_id])->groupBy('name')->select('name', DB::raw('count(*) as playbacks'))->orderBy('playbacks', 'DESC')->limit(10)->get();
+        $topTracksByAirTime = DB::table('playbackstats_tracks')->where('starttime', '>=', $subDaysTime)->where(['accountid' => $account_id])->groupBy('name')->select('name', DB::raw('sum(duration) as totalDuration'))->orderBy('totalDuration', 'DESC')->limit(10)->get();
+    }
+    return response()->json(['topTracksByAirTime' => $topTracksByAirTime, 'topTracksByPlayback' => $topTracksByPlayback, 'total_tracks' => $total_tracks, 'unique_tracks' => $unique_tracks, 'average_length' => $average_length, 'topTracksByAirTime' => $topTracksByAirTime, 'peak_listeners' => $peak_listeners, 'peak_track' => $peak_track, 'peak_time' => $peak_time]);
 });
 
 Route::post('/trylogin', [LoginController::class, 'TryLogin']);
