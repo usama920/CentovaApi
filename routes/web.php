788f9c600aa1ba08269fbe1216a661fc58eee30d
Usama Aslam <awesomeusama920@gmail.com>
2023-08-29 23:49:20 +0500

    new

--- routes/web.php
+++ routes/web.php
@@ -45,8 +45,6 @@ Route::post('/statistics/tracks', function (Request $request) {
     } else {
         $stats_results = DB::table('playbackstats_tracks')->where('starttime', '>=', $subDaysTime)->where(['accountid' => $account_id])->orderBy('listeners', 'DESC')->orderBy('duration', 'DESC')->chunk(100, function ($stats) {
             foreach ($stats as $stat) {
-                $new_array = ["name" => "test"];
-                array_push($playbackStats, $new_array);
             }
         });
         return response()->json(['playbackStats' => $playbackStats]);
