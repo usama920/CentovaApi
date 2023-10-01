e54ea45989bb0863f99458f42002e2049650185d
Usama Aslam <awesomeusama920@gmail.com>
2023-10-01 23:20:27 +0500

    new

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -62,7 +62,7 @@ class StatisticsController extends Controller
         $skip = 0;
         $playlists = [];
 
-        while ($skip < 4000) {
+        while ($skip < 3000) {
             $stats = PlaybackstatsTracks::whereBetween('starttime', [$startDate, $endDate])->where(['accountid' => $account_id])->orderBy('starttime', 'ASC')->skip($skip)->limit(500)->get();
             $skip += 500;
 
